0d1e9a88f0044278f66f58a6970047002a0948d4
ElenaH <elenah@interlink-ua.com>
2015-12-02 14:19:47 +0200

    Add entity

--- src/main/java/core/entity/Mark.java
+++ src/main/java/core/entity/Mark.java
@@ -1,7 +1,59 @@
 package core.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by employee on 12/2/15.
  */
+@Entity
+@Table(name = "marks")
 public class Mark {
+    @Id
+    @Column(name = "idStudent")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idStudent;
+
+    @Id
+    @Column(name = "idSubject")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idSubject;
+
+    @Column(name = "mark", nullable = false)
+    private int mark;
+
+
+    public Mark() {
+    }
+
+    public Mark(int idStudent, int idSubject, int mark) {
+        this.idStudent = idStudent;
+        this.idSubject = idSubject;
+        this.mark = mark;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public void setIdSubject(int idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public int getIdSubject() {
+        return idSubject;
+    }
+
+    public int getMark() {
+        return mark;
+
+    }
 }
--- src/main/java/core/entity/Student.java
+++ src/main/java/core/entity/Student.java
@@ -1,7 +1,42 @@
 package core.entity;
 
+import javax.persistence.*;
+
 /**
  * Created by employee on 12/2/15.
  */
+@Entity
+@Table(name="students")
 public class Student {
-}
+
+        @Id
+        @Column(name = "id")
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int id;
+
+        @Column(name = "name", length = 100, nullable = false)
+        private String name;
+
+
+        public Student(){}
+
+        public Student(String name){
+            this.name = name;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
